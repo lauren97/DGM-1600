ee536c7d5c831466f1d304a80a9bbde23fc11959
Lauren Grace <laurengrace97@gmail.com>
2017-08-31 11:15:23 -0600

    Edited Number Guessing Game

---
+++ NumberGuessingGame/Assets/NumberGuessing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGuessing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to The Number Guesser!");
+		print ("Choose a number and keep it secret. I will begin guessing soon.");
+
+		print ("The highest number you can pick is 100, and the lowest number you can pick is 1.");
+
+		print ("Is the number higher or lower than 50?");
+		print ("Up arrow for higher, Down for lower, Enter for Equal");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
