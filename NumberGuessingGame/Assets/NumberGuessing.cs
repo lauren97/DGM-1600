4841ad9df652309e307a468c94551b1eca9548f5
Lauren Grace <laurengrace97@gmail.com>
2017-09-12 11:09:46 -0600

    Added Visuals To Game

--- NumberGuessingGame/Assets/NumberGuessing.cs
+++ NumberGuessingGame/Assets/NumberGuessing.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumberGuessing : MonoBehaviour {
+
+	public Text theGuesser;
 	//variables
 	private int maxValue = 100, minValue = 1, guess = 50; 
 	public int count;
@@ -11,7 +14,14 @@ public class NumberGuessing : MonoBehaviour {
 	void Start ()
 	{
 		int guess = Random.Range(minValue, maxValue);
-	//Script
+
+		theGuesser.text = "\nWelcome! I am The Guesser. "
+			+ "\nChoose a number and keep it secret. I will being guessing soon. "
+			+ "\n\nThe highest number you can pick is " + maxValue
+			+ "\nThe lowest number you can pick is " + minValue
+			+"\nUp arrow for Higher, Down Arrow for Lower, and Enter/Return for equal."
+			+"\n\nIs the number lower or higher than " + guess;
+		
 		print ("Welcome to The Number Guesser!");
 		print ("Choose a number and keep it secret. I will begin guessing soon.");
 
