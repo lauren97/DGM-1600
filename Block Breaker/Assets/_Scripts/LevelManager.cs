e6a23e0f2c5f66b8e2f1b9b663873425311f6ce8
Lauren Grace <laurengrace97@gmail.com>
2017-10-05 11:12:11 -0600

    Added Ball to Block Breaker

--- Block Breaker/Assets/_Scripts/LevelManager.cs
+++ Block Breaker/Assets/_Scripts/LevelManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+    public void OpenLevel(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
     public void QuitGame()
     {
         print("Exit?");
