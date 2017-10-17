b9e0ffb4d8b5e7f3ffe97a080f726f6e8c34ecc9
Unknown <laurengrace97@gmail.com>
2017-10-17 11:26:43 -0600

    Fixed Game

---
+++ Block Breaker/Assets/_Scripts/Brick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+	public int health = 5;
+	void OnCollisionEnter2D (Collision2D collider)
+	{
+		health--;
+
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Block Breaker/Assets/_Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleHeight;
+
+	private Transform paddleTrans;
+
+	void Start ()
+	{
+		paddleTrans = gameObject.GetComponent<Transform> ();
+	}
+	void Update (){
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		//overrie vertical y position
+		mousePos.y = paddleHeight;
+		//override z depth position
+		mousePos.z = 0;
+		//apply transform information from mousePos
+		paddleTrans.position = mousePos;
+	}
+}
