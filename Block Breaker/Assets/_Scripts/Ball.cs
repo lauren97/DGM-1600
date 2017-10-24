43bdfbdfed35e7ab8a46a3c5d0b55a3cfb4f8bf2
Unknown <laurengrace97@gmail.com>
2017-10-24 11:36:04 -0600

    Added Walls and Changed Graphics on Bricks

---
+++ Block Breaker/Assets/_Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public GameObject paddle;
+	private bool launched = false;
+	private Rigidbody2D rigid;
+
+	private Vector3 paddleToBallVector; //distance from ball to paddle
+	// Use this for initialization
+
+	void Start () 
+	{
+		//Set the position
+		paddleToBallVector =this.transform.position - paddle.transform.position;
+		this.rigid = this.GetComponent<Rigidbody2D>();		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!launched)
+		{
+			//ball's position stays constantly on the paddle until game has started
+			this.transform.position = paddle.transform.position + paddleToBallVector;	
+		}
+
+		//if push start button
+		if(Input.GetMouseButtonDown(0))
+		{
+			//ball goes flying
+			rigid.velocity = new Vector2(4,20);
+			//launched = true
+			launched = true;
+
+		}
+
+	}
+}
--- Block Breaker/Assets/_Scripts/Brick.cs
+++ Block Breaker/Assets/_Scripts/Brick.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 	public int health = 5;
+	public Sprite[] state;
+	private int count = 0;
 	void OnCollisionEnter2D (Collision2D collider)
 	{
 		health--;
+		count++;
+
+		//change the picture
 
 		if (health <= 0) {
 			Destroy (this.gameObject);
 		}
-	}
+		GetComponent<SpriteRenderer>().sprite = state[count];}
 }
