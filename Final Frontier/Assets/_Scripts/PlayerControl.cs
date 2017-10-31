945e35d8db6fd2645c5f78eb482ab1769a8cd30c
Unknown <laurengrace97@gmail.com>
2017-10-31 11:30:46 -0600

    Created Final Frontier Game Project

---
+++ Final Frontier/Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+	//Instructions
+	//Add to Ship
+	//Move Ship with arrow keys
+
+	//Make speed adjustable in inspector
+	public float speed = 1;
+
+	// Use this for initialization
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Move Ship with Arrow Keys
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			//move player Up
+			print ("Up");
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			//move player Down
+			print ("Down");
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			//move player Left
+			print ("Left");
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			//move player Right
+			print ("Right");
+		}
+
+		
+	}
+}
