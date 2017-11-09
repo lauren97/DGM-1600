8ba855760788704ebcd7c7f3331cbd65bda3add7
Lauren Grace <laurengrace97@gmail.com>
2017-11-09 11:37:38 -0700

    Added Stars

---
+++ Final Frontier/Assets/_Scripts/Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public Transform objectToFollow;
+    public float zOffset;
+    // Use this for initialization
+
+    
+	void Update ()
+    {
+        //create temp vector3 to adjust z pos, and gameobject pos
+
+        Vector3 myPos = (gameObject.transform.position);
+        //adjust z position
+        myPos.z = zOffset;
+
+        //adjust gameObject position.
+        gameObject.transform.position = myPos;
+        
+	}
+}
