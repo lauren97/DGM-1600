dde4711d4690f1dbbdaf919f71158ebdacedaddb
Lauren Grace <laurengrace97@gmail.com>
2017-12-11 15:15:09 -0700

    Tried to do stuff

---
+++ Asteroid Belt/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int brickCount;
+	public void Start()
+	{
+		
+
+	}
+
+	public void OpenLevel(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+    public void QuitGame()
+    {
+        print("Exit?");
+        Application.Quit();
+    }
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void CheckBrickCount()
+	{
+		
+
+	}
+}
